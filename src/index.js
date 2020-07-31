f96761898f43b55fc2b345248e4458e949ac064d
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-31 16:54:51 -0700

    react

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+
+import App from "./components/App";
+import reducers from "./reducers"
