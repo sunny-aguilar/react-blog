e245d93d199c112caf3c5268ab5a48fb3575a7a9
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-31 17:09:00 -0700

    react

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import PostList from "./PostList";
 
 const App = () => {
-  return <div className="ui container">App</div>
+  return (
+    <div className="ui container"><PostList /></div>
+  );
 };
 
 export default App;
