44f0bfb0ce6869b1488f3ec31e83dd802edccee1
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-31 17:50:12 -0700

    react

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = async () => {
-  await jsonPlaceholder.get("./posts");
+  const response = await jsonPlaceholder.get("./posts");
 
   return {
     type: "FETCH_POSTS"
