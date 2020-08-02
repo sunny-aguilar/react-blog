054dfdbe9c2173a228ad304a7893e497fbbbf05e
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-01 23:09:08 -0700

    react

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -18,7 +18,7 @@ class PostList extends React.Component {
   render() {
     console.log(this.props.posts);
     // return <div className="ui relaxed divided list">{this.renderList()}</div>;
-    // return <div>Post List</div>;
+    return <div>Post List</div>;
   }
 }
 
