9f84e7440e75c625fef43d031b7df5c6bf592bd8
sunny-aguilar <sunny.aguilar@gmail.com>
2020-08-03 16:10:23 -0700

    Updates

---
+++ precache-manifest.e31f95fc3116dbd828bcdd018d297220.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b51455b0d4b9d7df4da3467f6b6f85",
+    "url": "/%3Crepo-name-here%3E/index.html"
+  },
+  {
+    "revision": "05d03a8267a77ebb5e1b",
+    "url": "/%3Crepo-name-here%3E/static/js/2.5b9caa7a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/%3Crepo-name-here%3E/static/js/2.5b9caa7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861c17a5de8a0bcd5faa",
+    "url": "/%3Crepo-name-here%3E/static/js/main.5eae3668.chunk.js"
+  },
+  {
+    "revision": "ac8687107eb79b294f82",
+    "url": "/%3Crepo-name-here%3E/static/js/runtime-main.c0f3301b.js"
+  }
+]);
